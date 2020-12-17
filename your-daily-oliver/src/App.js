c032f515702e96ecb23934191b7d413a1a3f6b28
NekoCarreraDesigns <denvermaas@gmail.com>
2020-12-16 22:45:53 -0700

    added header, and components folder

--- your-daily-oliver/src/App.js
+++ your-daily-oliver/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "./Components/Header/index"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       
     </div>
   );
---
+++ your-daily-oliver/src/Components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+function Header () {
+    return (
+        <div>
+            <header className="page-header">
+            <h1><i class="fas fa-paw"></i>{' '}Your Daily Oliver{' '}<i class="fas fa-paw"></i></h1>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
